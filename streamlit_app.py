e42d33185f514e14103604bfd2210837726c2470
Dash Desai <iamontheinet@gmail.com>
2022-12-07 16:06:27 -0800

    Refactored code and updated layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,20 +22,13 @@ def get_packages_data(url):
     packages = soup.find("table").find_all("tr")
     return packages
 
-def write_package_details(col, package):
+def write_package_details(col, package, linux_64, linux_aarch64, osx_64, osx_arm64, win_64, noarch):
     p_name = package.find("td", class_ = 'packagename').text
     p_version = package.find("td", class_ = 'version').text
     p_license = package.find("td", class_ = 'license').text
     p_summary = package.find("td", class_ = 'summary').text
     p_docs_link = package.find_all("td")[2].a
     p_gits_link = package.find_all("td")[3].a
-
-    linux_64 = package.find_all("td")[5].text
-    linux_aarch64 = package.find_all("td")[6].text
-    osx_64 = package.find_all("td")[7].text
-    osx_arm64 = package.find_all("td")[8].text
-    win_64 = package.find_all("td")[9].text
-    noarch = package.find_all("td")[10].text
 
     col.subheader(p_name)
     doc_link = f"<a href=\'{p_docs_link['href']}\' target=\'_blank\'>Docs</a>" if p_docs_link else "Docs: N/A"
@@ -44,48 +37,61 @@ def write_package_details(col, package):
     col.caption(f"Latest version: {p_version} | License: {p_license if p_license else 'N/A'} | {docs_and_git_links}", unsafe_allow_html = True)
     col.caption(f"linux-64: {'✅' if linux_64 else 'N/A'} | linux-aarch64: {'✅' if linux_aarch64 else 'N/A'} | osx-64: {'✅' if osx_64 else 'N/A'} | osx-arm64: {'✅' if osx_arm64 else 'N/A'} | win-64: {'✅' if win_64 else 'N/A'} | noarch: {'✅' if noarch else 'N/A'}")
     col.write(p_summary)
+    return 1
 
 def display_data_as_blocks(packages, this_is_a_test=True):
     col1, col2, col3 = st.columns(3, gap='small')
     p_container = st.container()
     col_index = 0
+    p_index = 0
     for i in range(0,total_packages):
         package = packages[i]
         package_name = package.find("td", class_ = 'packagename')
         if package_name:
-            with p_container:
-                col = col1 if col_index == 1 else col2 if col_index == 2 else col3
-                write_package_details(col, package)               
+            linux_64 = package.find_all("td")[5].text
+            linux_aarch64 = package.find_all("td")[6].text
+            osx_64 = package.find_all("td")[7].text
+            osx_arm64 = package.find_all("td")[8].text
+            win_64 = package.find_all("td")[9].text
+            noarch = package.find_all("td")[10].text
 
-            if (i % 3) == 0:
-                col1, col2, col3 = st.columns(3, gap='small')
-                p_container = st.container()
-                col_index = 0
-            else:
-                col_index += 1
+            # 'linux-64','linux-aarch64','osx-64','osx-arm64','win-64','noarch'
+            if ((linux_64 and 'linux-64' in p_selected_os) or \
+                (linux_aarch64 and 'linux-aarch64' in p_selected_os) or \
+                (osx_64 and 'osx-64' in p_selected_os) or \
+                (osx_arm64 and 'osx-arm64' in p_selected_os) or \
+                (win_64 and 'win-64' in p_selected_os) or \
+                (noarch and 'noarch' in p_selected_os)):
 
-        if this_is_a_test and i == 9:
+                with p_container:
+                    col = col1 if col_index == 1 else col2 if col_index == 2 else col3
+                    p_index += write_package_details(col, package, linux_64, linux_aarch64, osx_64, osx_arm64, win_64, noarch)               
+
+                if (p_index % 3) == 0:
+                    col1, col2, col3 = st.columns(3, gap='small')
+                    p_container = st.container()
+                    col_index = 0
+                else:
+                    col_index += 1
+
+        if this_is_a_test and p_index == 9:
             break;
 
-# Set the URL 
 repo_anaconda_com_url = 'https://repo.anaconda.com/pkgs/snowflake/'
-with st.spinner("Please wait while I load the packages..."):
-    packages = get_packages_data(repo_anaconda_com_url)
-
+packages = get_packages_data(repo_anaconda_com_url)
 total_packages = len(packages)
 
-st.header(f"Snowflake Snowpark for Python -- Conda Channel")
+st.header(f"Snowpark for Python -- Python Packages in Snowflake Conda Channel")
+st.caption(f"App developed by [Dash](https://twitter.com/iamontheinet)")
 st.write(f"The Snowflake Conda channel is maintained and supported by Anaconda. It has been built for use with <a href='https://www.snowflake.com/snowpark-for-python/'>Snowpark for Python</a>. By accessing or using the contents herein, you acknowledge and agree that you have read, understood, and agree to be bound by the <a href='https://legal.anaconda.com/policies/en/?name=terms-of-service#embedded-end-customer-terms' target='_blank'>Embedded End Customer Terms</a> to <a href='https://legal.anaconda.com/policies/en/?name=terms-of-service' target='_blank'>Anaconda's Terms of Service</a>.", unsafe_allow_html=True)
 st.write(f"Total packages in this channel as of today = **{total_packages}**. These curated packages are available for you to use in <a href='https://docs.snowflake.com/en/developer-guide/snowpark/python/creating-udfs.html'>User-Defined Functions</a>, <a href='https://docs.snowflake.com/en/developer-guide/udf/python/udf-python-tabular-functions.html'>User-Defined Table Functions</a>, and <a href='https://docs.snowflake.com/en/sql-reference/stored-procedures-python.html'>Stored Procedures</a> after <a href='https://docs.snowflake.com/en/developer-guide/udf/python/udf-python-packages.html#getting-started'>accepting the terms</a>.", unsafe_allow_html=True)
-st.caption(f"App developed by [Dash](https://twitter.com/iamontheinet)")
-st.markdown("---")
 
-with st.spinner("Please wait while I organize the packages..."):
-    display_data_as_blocks(packages,False)
-
+p_os = ['linux-64','linux-aarch64','osx-64','osx-arm64','win-64','noarch']
+p_selected_os = st.multiselect('Filter packages by OS. (Note: It applies OR operator)',p_os, default = ['linux-64'],label_visibility='visible')
+st.markdown("___")
+display_data_as_blocks(packages,False)
 st.markdown("---")
 st.caption(f"Conda Channel Source: https://repo.anaconda.com/pkgs/snowflake/")
-
 
 # def organize_data(packages):
 #     package_names, package_summaries, package_versions, package_licenses, os1, os2, os3, os4, os5, os6, docs, gits = [],[],[],[],[],[],[],[],[],[],[],[]
