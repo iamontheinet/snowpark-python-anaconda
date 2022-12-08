72e356eda1f90f29e27056986101d59cb88340a9
Dash Desai <iamontheinet@gmail.com>
2022-12-08 10:46:15 -0800

    Added GitHub repo link in the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,7 @@ st.set_page_config(
      layout="wide",
      menu_items={
          'Get Help': 'https://developers.snowflake.com',
-         'About': "This app is developed by dash.desai@snowflake.com"
-         'View Source': "https://github.com/iamontheinet/snowpark-python-anaconda"
+         'About': "The source code for this application can be accessed on GitHub https://github.com/iamontheinet/snowpark-python-anaconda"
      }
 )
 
