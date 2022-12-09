ce769ea356c5bb6ba8d4c55b6c10c3c461f81757
Dash Desai <iamontheinet@gmail.com>
2022-12-09 11:59:35 -0800

    Refactored code to load the packages in 'pages' to speed up the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,10 @@ st.markdown("""
         font-size: 30px;
         color: #FFFFFF;
     }
+    [data-testid=stSidebar] {
+        background-color: rgb(129, 164, 182);
+        color: #FFFFFF;
+    }
     </style>
 """, unsafe_allow_html=True)
 
@@ -54,13 +58,13 @@ def write_package_details(col, package, linux_64, linux_aarch64, osx_64, osx_arm
     col.caption(f"Latest version: {p_version} | License: {p_license if p_license else 'N/A'} | {docs_and_git_links}", unsafe_allow_html = True)
     col.caption(f"linux-64: {'✅' if linux_64 else 'N/A'} | linux-aarch64: {'✅' if linux_aarch64 else 'N/A'} | osx-64: {'✅' if osx_64 else 'N/A'} | osx-arm64: {'✅' if osx_arm64 else 'N/A'} | win-64: {'✅' if win_64 else 'N/A'} | noarch: {'✅' if noarch else 'N/A'}")
     col.write(p_summary)
-    return 1
 
-def display_package_as_blocks(packages, this_is_a_test=False):
+def display_all_packages_as_blocks(this_is_a_test=False):
     col1, col2, col3 = st.columns(3, gap='small')
     p_container = st.container()
     col_index = 0
     p_index = 0
+    total_packages = len(packages)
     for i in range(0,total_packages):
         package = packages[i]
         package_name = package.find("td", class_ = 'packagename')
@@ -74,10 +78,10 @@ def display_package_as_blocks(packages, this_is_a_test=False):
 
             with p_container:
                 col = col1 if col_index == 1 else col2 if col_index == 2 else col3
-                p_index += write_package_details(col, package, linux_64, linux_aarch64, osx_64, osx_arm64, win_64, noarch)               
+                write_package_details(col, package, linux_64, linux_aarch64, osx_64, osx_arm64, win_64, noarch)   
+                p_index += 1            
 
             if (p_index % 3) == 0:
-                # Stare/create a new row after writing out 3 packages in a row
                 col1, col2, col3 = st.columns(3, gap='small')
                 p_container = st.container()
                 col_index = 0
@@ -86,6 +90,45 @@ def display_package_as_blocks(packages, this_is_a_test=False):
 
         if this_is_a_test and p_index == 9:
             break;
+
+def display_packages_as_blocks():
+    col1, col2, col3 = st.columns(3, gap='small')
+    p_container = st.container()
+    col_index = 0
+
+    if 'p_start' not in st.session_state:
+        st.session_state['p_start'] = 1
+        st.session_state['p_end'] = 400
+        st.session_state['p_total'] = len(packages)
+    else:
+        st.session_state['p_start'] += 400
+        st.session_state['p_end'] = st.session_state['p_start'] + 400
+
+    p_total = st.session_state['p_total']
+    p_start = st.session_state['p_start']
+    p_end = st.session_state['p_end'] if st.session_state['p_end'] < p_total else p_total
+
+    for i in range(p_start,p_end):
+        package = packages[i]
+        package_name = package.find("td", class_ = 'packagename')
+        if package_name:
+            linux_64 = package.find_all("td")[5].text
+            linux_aarch64 = package.find_all("td")[6].text
+            osx_64 = package.find_all("td")[7].text
+            osx_arm64 = package.find_all("td")[8].text
+            win_64 = package.find_all("td")[9].text
+            noarch = package.find_all("td")[10].text
+
+            with p_container:
+                col = col1 if col_index == 1 else col2 if col_index == 2 else col3
+                write_package_details(col, package, linux_64, linux_aarch64, osx_64, osx_arm64, win_64, noarch)             
+
+            if (i % 3) == 0:
+                col1, col2, col3 = st.columns(3, gap='small')
+                p_container = st.container()
+                col_index = 0
+            else:
+                col_index += 1
 
 with st.container():
     col1,col2,_,_ = st.columns([1,14,1,1],gap="large")
@@ -98,10 +141,26 @@ st.write(f"This application lists the Python packages from the Snowflake Conda c
 
 repo_anaconda_com_url = 'https://repo.anaconda.com/pkgs/snowflake/'
 packages = get_packages_data(repo_anaconda_com_url)
-total_packages = len(packages)
 st.write(f"The curated packages in Snowflake Conda channel are available for you to use in <a href='https://docs.snowflake.com/en/developer-guide/snowpark/python/creating-udfs.html'>User-Defined Functions</a>, <a href='https://docs.snowflake.com/en/developer-guide/udf/python/udf-python-tabular-functions.html'>User-Defined Table Functions</a>, and <a href='https://docs.snowflake.com/en/sql-reference/stored-procedures-python.html'>Stored Procedures</a> after you <a href='https://docs.snowflake.com/en/developer-guide/udf/python/udf-python-packages.html#getting-started'>accept the terms</a>.", unsafe_allow_html=True)
-st.caption(f"Note: For a complete list and latest version of packages available, run query *SELECT package_name, max(version) FROM \"INFORMATION_SCHEMA\".\"PACKAGES\" WHERE LANGUAGE = \"pytho\" GROUP BY package_name ORDER BY 1;* in Snowsight in your Snowflake account.")
 
+st.markdown("___")
+
+page_names_to_funcs = {
+    "Load packages 1 to 400": display_packages_as_blocks,
+    "Load packages 401 to 800": display_packages_as_blocks,
+    "Load packages 801 to 1200": display_packages_as_blocks,
+    "Load packages 1201 to 1600": display_packages_as_blocks   
+}
+
+selected_page = st.sidebar.selectbox("Select range", page_names_to_funcs.keys())
+st.sidebar.markdown("---")
+st.sidebar.write(f"Note: For a complete list and latest version of packages available, run query *SELECT package_name, max(version) FROM \"INFORMATION_SCHEMA\".\"PACKAGES\" WHERE LANGUAGE = \"python\" GROUP BY package_name ORDER BY 1;* in Snowsight in your Snowflake account.")
+page_names_to_funcs[selected_page]()
+
+st.markdown("---")
+st.caption(f"Conda Channel Source: https://repo.anaconda.com/pkgs/snowflake/")
+
+# -------------
 # st.markdown("___")
 # st.caption("If you're developing locally, you can filter packages by platform. (Note: It applies an OR operator between the selected platforms.)")
 # with st.container():
@@ -111,8 +170,3 @@ st.caption(f"Note: For a complete list and latest version of packages available,
 #     for i, (col,os) in enumerate(zip(cols,oss)):
 #         with col:
 #             os_chk.append(st.checkbox(os,value=True) if i == 0 else st.checkbox(os))
-
-st.markdown("___")
-display_package_as_blocks(packages)
-st.markdown("---")
-st.caption(f"Conda Channel Source: https://repo.anaconda.com/pkgs/snowflake/")
